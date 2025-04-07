e24e2ceb2230358f6533ba06730137e1c71753aa
Kiet <kietnguyentuan911@gmail.com>
2025-04-07 19:04:45 +0700

    chores: config ignore swagger file

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'node_modules/', 'dist/', './github/'],
-  skipFiles: ['**/metadata.ts'],
+  ignorePatterns: ['.eslintrc.js', 'node_modules/', 'dist/', './github/', 'src/metadata.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
