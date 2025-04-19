873552203ea04c1b3464c18d8150211dc827f629
Kiet <kietnguyentuan911@gmail.com>
2025-04-19 16:10:52 +0700

    feat: update category schema

--- prisma/migrations/20250417221557_change_field_type_of_category/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `url` on the `category` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "category" DROP COLUMN "url",
-ADD COLUMN     "urls" TEXT[];
