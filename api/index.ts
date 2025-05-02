c4ec6af42da3acb0761671156fe7028477dbb181
ItsMeNozo <dungtuyetngoc@gmail.com>
2025-05-02 14:40:35 +0700

    update vercel

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,23 @@
-// api/index.ts
-import type { VercelRequest, VercelResponse } from '@vercel/node';  
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../src/app.module';
-import serverlessExpress from '@codegenie/serverless-express';      
+import '../src/main'
+// // api/index.ts
+// import type { VercelRequest, VercelResponse } from '@vercel/node';  
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from '../src/app.module';
+// import serverlessExpress from '@codegenie/serverless-express';      
 
-let cached: ReturnType<typeof serverlessExpress>;
+// let cached: ReturnType<typeof serverlessExpress>;
 
-export default async function handler(
-  req: VercelRequest,
-  res: VercelResponse,
-) {
-  if (!cached) {
-    const app = await NestFactory.create(AppModule);
-    await app.init();                   
-    cached = serverlessExpress({
-      app: app.getHttpAdapter().getInstance(),
-    });
-  }
-  return cached(req, res);
-}
+// export default async function handler(
+//   req: VercelRequest,
+//   res: VercelResponse,
+// ) {
+//   if (!cached) {
+//     const app = await NestFactory.create(AppModule);
+//     await app.init();                   
+//     cached = serverlessExpress({
+//       app: app.getHttpAdapter().getInstance(),
+//     });
+//   }
+//   return cached(req, res);
+// }
+
