9095c999830590676de25fa462ac7c63b733196b
Kiet <kietnguyentuan911@gmail.com>
2025-05-10 18:15:58 +0700

    feat: handle prompt not found and add try catch

--- src/art-generation/prompt.service.ts
+++ src/art-generation/prompt.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { plainToInstance } from "class-transformer";
 import { PrismaService } from "src/prisma.service";
 import { PromptHistoryDto } from "./dto/response/prompt-history.dto";
 import { UpdatePromptHistoryDto } from "./dto/request/update-prompt-history.dto";
+import { TryCatch } from "src/common/try-catch.decorator";
 
 @Injectable()
 export class PromptService {
@@ -10,6 +11,7 @@ export class PromptService {
     private readonly prismaService: PrismaService,
   ) {}
 
+  @TryCatch()
   async getPromptHistory(userId: string): Promise<PromptHistoryDto[]> {
     const promptHistory = await this.prismaService.artGeneration.findMany({
       where: {
@@ -23,10 +25,19 @@ export class PromptService {
     return plainToInstance(PromptHistoryDto, promptHistory);
   }
 
+  @TryCatch()
   async updatePromptHistory(
     promptId: number,
     updatePromptHistoryDto: UpdatePromptHistoryDto,
   ): Promise<PromptHistoryDto> {
+    const existingPromptHistory = await this.prismaService.artGeneration.findUnique({
+      where: {
+        id: promptId,
+      },
+    });
+    if (!existingPromptHistory) {
+      throw new BadRequestException(`Prompt history with id = ${promptId} not found`);
+    }
     const updatedPromptHistory = await this.prismaService.artGeneration.update({
       where: {
         id: promptId,
