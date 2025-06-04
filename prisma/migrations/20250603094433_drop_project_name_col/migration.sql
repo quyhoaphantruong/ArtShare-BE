e5eb5b74b99e14e529491dada6f2914e887ff62d
liwjln <phamtrantuantu17@gmail.com>
2025-06-04 07:50:21 +0700

    chore: drop project name

---
+++ prisma/migrations/20250603094433_drop_project_name_col/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `project_name` on the `auto_project` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "auto_project" DROP COLUMN "project_name";
